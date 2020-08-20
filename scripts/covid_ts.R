81d8adecf3ce1f79a7697ca79805789aa198787b
Lucas Moschen <lucas.machadomoschen@gmail.com>
2020-08-19 23:49:28 -0300

    Deleted unecessary

--- scripts/covid_ts.R
+++ scripts/covid_ts.R
@@ -1,10 +1,14 @@
+#Importando bibliotecas
+setwd("~/OneDrive/FGV/Eletivas ou Complementares/Séries Temporais")
 library(zoo)
 
-covid <- readxl::read_xlsx('/home/lucasmoschen/Downloads/covidrj.xlsx')
-covid.df <- zoo(diff(covid$deaths, 1), calendar = covid$date[-1], frequency = 7)
+#1. A séries é estacionária?
+#2. Existe alguma sazonalidade?
+#3. Existe alguma tendência?
+#4. O modelo fica melhor na escala logaritmica? (modelo multiplicativo)
+#5. O teste de sequencias aponta tendência?
+#6. Algum dos testes estudados em sala aponta sazonalidade?
+
+covid <- readxl::read_xlsx('dados/covidrj.xlsx')
+covid.df <- zoo(diff(covid$deaths, 1), order.by = covid$date[-1], frequency = 7)
 plot(covid.df)
-
-Q <- rep(seq(1,7), length(covid.df)/7)
-dec <- decompose(factor(covid.df))
-
-kruskal.test()
